440b844b72c7ebc0d3e92932393da67305ab1d65
Gina Sprint <sprint@gonzaga.edu>
2021-09-20 20:35:21 -0700

    update Book toString()

--- src/Book.java
+++ src/Book.java
@@ -32,6 +32,6 @@ public class Book extends Publication {
 
     @Override
     public String toString() {
-        return super.toString();
+        return super.toString() + ", " + author;
     }
 }
